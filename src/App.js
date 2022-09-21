f9e64497581d9aa4a32d6685996969002e1f3ee5
abhishekkr1025 <74841098+abhishekkr1025@users.noreply.github.com>
2022-09-21 14:10:23 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import ReactDOM from 'react-dom';
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+function App(){
+  const[name,setName] = useState('');
+  const[email,setEmail] = useState('');
+  const[password,setPassword] = useState('')
+  const[confPassword,setConfPassword] = useState('')
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleConfPasswordChange =(e)=>{
+    setConfPassword(e.target.value);
+  }
+  const handleSubmit = (e) => {
+    if(password!=confPassword){
+      alert('email does not match');
+    } else{
+      alert('A form was submitted with '+name+" and "+"email "+email);
+    }
+
+    e.preventDefault();
+  }
+ return(
+   <div className = "App">
+     <header className = "App-header">
+      <form onSubmit = {(e) =>{handleSubmit(e)}}>
+      <h1>Sign up form</h1>
+      <label>Name: </label>
+      <input type="text" value = {name} onChange = {(e)=>{handleChange(e)}}/><br/><br/>
+      <label>Email: </label>
+      <input type="email" value = {email} onChange = {(e)=>{handleEmailChange(e)}}/><br/><br/>
+      <label>Password: </label>
+      <input type="password" value = {password} onChange = {(e)=>{handlePasswordChange(e)}}/><br/><br/>
+      <label>Confrim Password: </label>
+      <input type="text" value = {confPassword} onChange = {(e)=>{handleConfPasswordChange(e)}}/><br/><br/>
+      <input type = "submit" value = "Submit"/>
+      </form>
+     </header>
+   </div>
+ );
 }
+
+export default App;
